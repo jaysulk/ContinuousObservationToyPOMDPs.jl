ec36fa96d7cb9698ea5fb605cb3c54aa20828aa5
Jason A. Sulskis <jason.sulskis@gmail.com>
2022-10-25 21:04:41 -0700

    Removed simplelightdark

--- src/ContinuousObservationToyPOMDPs.jl
+++ src/ContinuousObservationToyPOMDPs.jl
@@ -13,16 +13,6 @@ using Distributions
 using Base.Iterators
 
 export
-    SimpleLightDark,
-    DSimpleLightDark,
-    LDHeuristic,
-    LDHSolver,
-    LDSide,
-    LDSidePolicy
-
-include("simple_lightdark.jl")
-
-export
     COTigerPOMDP,
     DOTigerPOMDP,
     TimedCOTigerPOMDP,
